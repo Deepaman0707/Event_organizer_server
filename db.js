17562b017ba89df40c5b6fdd7e4690321fc3571a
Aman Deep <deep.aman07.ad@gmail.com>
2021-08-18 13:07:36 +0530

    .

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
