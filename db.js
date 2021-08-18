3872c1f99be3b69e1f7ec3958010ffec3a689a6a
Aman Deep <deep.aman07.ad@gmail.com>
2021-08-18 11:16:48 +0530

    database

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
-const Pool = require('pg').Pool
-
+const { Pool } = require('pg')
+const isProduction = process.env.NODE_ENV === 'production'
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: '123456789',
-  port: 5432,
-  database: 'events',
+  connectionString: isProduction
+    ? process.env.DATABASE_URL // Heroku will supply us with a string called DATABASE_URL for the connectionString,
+    : connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 })
 
-module.exports = pool
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+}
